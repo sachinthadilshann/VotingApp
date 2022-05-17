511355abadf8584ab4c5bf49f281f50585c5ce00
Sachintha <sachinthadilshan1120@gmail.com>
2022-05-17 14:33:04 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/ic_launcher-playstore.png
    #	app/src/main/res/mipmap-hdpi/ic_launcher.png
    #	app/src/main/res/mipmap-hdpi/ic_launcher_round.png
    #	app/src/main/res/mipmap-mdpi/ic_launcher.png
    #	app/src/main/res/mipmap-mdpi/ic_launcher_round.png
    #	app/src/main/res/mipmap-xhdpi/ic_launcher.png
    #	app/src/main/res/mipmap-xhdpi/ic_launcher_round.png
    #	app/src/main/res/mipmap-xxhdpi/ic_launcher.png
    #	app/src/main/res/mipmap-xxhdpi/ic_launcher_round.png
    #	app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
    #	app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png

--- app/src/main/java/com/radproject/votingapp/MainActivity.java
+++ app/src/main/java/com/radproject/votingapp/MainActivity.java
@@ -3,12 +3,45 @@ package com.radproject.votingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int GALLERY_INTENT_CODE = 1023 ;
+    TextView fullName,email,phone,verifyMsg;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+    Button resendCode;
+    Button resetPassLocal,changeProfileImage;
+    FirebaseUser user;
+    ImageView profileImage;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        phone = findViewById(R.id.profilePhone);
+        fullName = findViewById(R.id.profileName);
+        email    = findViewById(R.id.profileEmail);
+        resetPassLocal = findViewById(R.id.resetPasswordLocal);
+
+        profileImage = findViewById(R.id.profileImage);
+        changeProfileImage = findViewById(R.id.changeProfile);
+
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
     }
 }
